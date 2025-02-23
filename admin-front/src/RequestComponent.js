41ea764743df776f7e4eb46b5330c63057dd6753
Somi khalili <smy.khalili@gmail.com>
2025-02-22 21:29:21 -0500

    admin-front

--- admin-front/src/RequestComponent.js
+++ admin-front/src/RequestComponent.js
@@ -1,17 +1,35 @@
 import React from "react";
+import "./styles.css";
 
 const RequestComponent = ({ request }) => {
   return (
-    <div className="border-b p-2">
+    <div>
       <strong>Name:</strong> {request.name || "N/A"} <br />
       <strong>Application:</strong> {request.application} <br />
-      <strong>Selected Answers:</strong>{" "}
-      {JSON.stringify(request.selectedAnswers)} <br />
-      <strong>Documents:</strong> {request.documents.join(", ")} <br />
-      <strong>Charges:</strong> {request.charges.join(", ")} <br />
-      <strong>Rule:</strong> {request.rule} <br />
+      {/* Display Selected Answers in a readable format */}
+      <strong>Selected Answers:</strong>
+      {request.selectedAnswers &&
+      Object.keys(request.selectedAnswers).length > 0 ? (
+        <ul className="custom-list">
+          {Object.entries(request.selectedAnswers).map(([key, value]) => (
+            <li key={key}>
+              <strong>{key}:</strong> {value}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <span> N/A</span>
+      )}
+      <br />
+      <strong>Documents:</strong>{" "}
+      {request.documents.length > 0 ? request.documents.join(", ") : "N/A"}{" "}
+      <br />
+      <strong>Charges:</strong>{" "}
+      {request.charges.length > 0 ? request.charges.join(", ") : "N/A"} <br />
+      <strong>Rule:</strong> {request.rule || "N/A"} <br />
       <strong>Created At:</strong>{" "}
-      {new Date(request.createdAt).toLocaleString()} <br />
+      {request.createdAt ? new Date(request.createdAt).toLocaleString() : "N/A"}{" "}
+      <br />
     </div>
   );
 };
